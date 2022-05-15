aba8417db1eb0e747b1426e5355d071c6a91a7c6
Santiago Barquero <sbarquero@gmail.com>
2022-05-15 21:33:15 +0200

    Add documentation link in root page

--- src/controllers/index.controller.ts
+++ src/controllers/index.controller.ts
@@ -3,7 +3,10 @@ import { NextFunction, Request, Response } from 'express';
 class IndexController {
   public index = (req: Request, res: Response, next: NextFunction) => {
     try {
-      res.sendStatus(200);
+      res.status(200).send(`
+        <p><strong>MyRoute API</strong> is running</p>
+        <p><a href="./api-docs">See API documentación</a></p>
+      `);
     } catch (error) {
       next(error);
     }
