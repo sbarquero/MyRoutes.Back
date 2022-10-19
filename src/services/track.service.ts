4a4086c0fa766f911d14a2c7df40d8b69fbba54e
Santiago Barquero <sbarquero@gmail.com>
2022-10-19 19:53:17 +0200

    Change old versión of xmldom package to @xmldom/xmldom

--- src/services/track.service.ts
+++ src/services/track.service.ts
@@ -1,4 +1,4 @@
-import { DOMParser } from 'xmldom';
+import { DOMParser } from '@xmldom/xmldom';
 import * as tj from '@tmcw/togeojson';
 
 import { CreateTrackDto, UpdateTrackDto } from '@/dtos/track.dto';
