81d1d11327e291a12bc4da4d94b319bb8245acf0
Santiago Barquero <sbarquero@gmail.com>
2022-11-20 10:53:29 +0100

    Add automatic redirection from index page to the api docs page in Swagger

--- src/controllers/index.controller.ts
+++ src/controllers/index.controller.ts
@@ -4,8 +4,15 @@ class IndexController {
   public index = (req: Request, res: Response, next: NextFunction) => {
     try {
       res.status(200).send(`
-        <p><strong>MyRoute API</strong> is running</p>
-        <p><a href="./api-docs">See API documentación</a></p>
+        <head>
+          <meta http-equiv="refresh" content="5; url='http://localhost:3000/api-docs/'" />
+        </head>
+        <body>
+          <h1><strong>MyRoute API</strong> is running</h1>
+
+          <p>The API documentation page will be loaded after 5 seconds...</p>
+          <p><a href="./api-docs">See API documentation now</a></p>
+        </body>
       `);
     } catch (error) {
       next(error);
